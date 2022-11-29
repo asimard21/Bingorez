959babb340fe3fa0d36c918ada6088cadbe145ed
asimard21 <100657046+asimard21@users.noreply.github.com>
2022-11-28 23:27:06 -0500

    add responsive desing on landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export const Index = () => {
     return (
-        <div className='center-flex flex-column all-height'>
+        <div className='main-container'>
             <Link to='/game'><button className='main'>Débuter une partie</button></Link>
             <Link to='/card'><button className='main'>Générer une carte</button></Link>
         </div>
